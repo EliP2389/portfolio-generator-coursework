5f0e9646ccb3e3a17943243581015d3eda7b5e56
Eli Pagan <mbk8689@gmail.com>
2021-11-23 20:57:56 -0500

    moduel.exports, created new file

---
+++ app.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [userName, github] = profileDataArgs;
+
+
+
+fs.writeFile('index.html', generatePage(userName, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
